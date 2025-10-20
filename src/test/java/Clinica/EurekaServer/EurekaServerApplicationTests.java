3a693ed4abd574484a5701d5b70b3b1fe4523cda
Kevin Broncano <kevinbroncanop@gmail.com>
2025-10-20 18:14:38 -0500

    Creación de EurekaServer

---
+++ src/test/java/Clinica/EurekaServer/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package Clinica.EurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
